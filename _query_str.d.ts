2a46c688044737134c6a7a99365234e4613fb16f
Brian Takita <brian.takita@gmail.com>
2020-08-05 15:54:09 -0700

    feat: _query_str: prefix argument

--- _query_str.d.ts
+++ _query_str.d.ts
@@ -1,2 +1,2 @@
-export declare function _query_str(query?: {}): string;
+export declare function _query_str(query?: {}, prefix?: string): string;
 export declare const _str__query: typeof _query_str;
--- _query_str.ts
+++ _query_str.ts
@@ -1,10 +1,10 @@
-export function _query_str(query = {}) {
+export function _query_str(query = {}, prefix='?') {
 	const query_str_a1 = []
 	for (let key in query) {
 		const value = query[key]
 		const value__ = value == null ? '' : value
 		query_str_a1.push(`${encodeURIComponent(key)}=${encodeURIComponent(value__)}`)
 	}
-	return query_str_a1.length ? `?${query_str_a1.join('&')}` : ''
+	return query_str_a1.length ? `${prefix}${query_str_a1.join('&')}` : ''
 }
 export const _str__query = _query_str
