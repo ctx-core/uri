41c85810f213fd4a4861edeb6a27a41a4ee55bc2
Brian Takita <brian.takita@gmail.com>
2020-09-06 10:06:01 -0700

    fix: join_url: single '/' when segment leads with a '/'

--- join_url.ts
+++ join_url.ts
@@ -1,7 +1,7 @@
 export function join_url(url_segment_a1:(string|number|boolean)[]) {
 	return url_segment_a1.reduce((url, url_segment)=>{
 		const url_segment_str = url_segment.toString()
-		if (~['?', '&'].indexOf(url_segment_str.slice(0, 1))) {
+		if (~['/', '?', '&'].indexOf(url_segment_str.slice(0, 1))) {
 			return `${url}${url_segment_str}`
 		} else {
 			return `${url}/${url_segment_str}`
