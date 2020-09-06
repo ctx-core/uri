254dd3c9f9e05f99abcb1cffcf03d04222e65f3d
Brian Takita <brian.takita@gmail.com>
2020-09-06 08:42:46 -0700

    BREAKING CHANGE: _query_str: query_type = string|Record<string, string|number|boolean>

--- _query_str.ts
+++ _query_str.ts
@@ -1,4 +1,4 @@
-export function _query_str(query:string|any = '', prefix = '?') {
+export function _query_str(query:query_type = '', prefix = '?') {
 	if (typeof query === 'string') return `${prefix}${query}`
 	const query_str_a1 = []
 	for (let key in query) {
@@ -9,3 +9,4 @@ export function _query_str(query:string|any = '', prefix = '?') {
 	return query_str_a1.length ? `${prefix}${query_str_a1.join('&')}` : ''
 }
 export const _str__query = _query_str
+export type query_type = string|Record<string, string|number|boolean>
