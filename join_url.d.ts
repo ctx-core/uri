efbf5a177ec12572b3c230b82bffa85727531d36
Brian Takita <brian.takita@gmail.com>
2020-09-06 09:49:55 -0700

    fix: join_url: return string

--- join_url.d.ts
+++ join_url.d.ts
@@ -1,1 +1,1 @@
-export declare function join_url(url_segment_a1: (string | number | boolean)[]): void;
+export declare function join_url(url_segment_a1: (string | number | boolean)[]): string | number | boolean;
--- join_url.ts
+++ join_url.ts
@@ -1,5 +1,5 @@
 export function join_url(url_segment_a1:(string|number|boolean)[]) {
-	url_segment_a1.reduce((url, url_segment)=>{
+	return url_segment_a1.reduce((url, url_segment)=>{
 		const url_segment_str = url_segment.toString()
 		if (~['?', '&'].indexOf(url_segment_str.slice(0, 1))) {
 			return `${url}${url_segment_str}`
