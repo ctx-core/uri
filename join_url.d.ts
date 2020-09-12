48f1f7de8a76e0828ba2b7c3bd4e1885710d7dbe
Brian Takita <brian.takita@gmail.com>
2020-09-11 19:06:16 -0700

    import type for type imports

--- join_url.d.ts
+++ join_url.d.ts
@@ -1,2 +1,2 @@
-import { url_segment_type } from './url_segment_type';
+import type { url_segment_type } from './url_segment_type';
 export declare function join_url(url_segment_a1: url_segment_type[]): string;
--- join_url.ts
+++ join_url.ts
@@ -1,4 +1,4 @@
-import { url_segment_type } from './url_segment_type'
+import type { url_segment_type } from './url_segment_type'
 export function join_url(url_segment_a1:url_segment_type[]) {
 	return url_segment_a1.reduce((url, url_segment)=>{
 		const url_segment_str = url_segment.toString()
