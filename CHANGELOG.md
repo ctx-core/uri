1bac8da4a92cec6325a71609f79b436d57d3c47d
Brian Takita <brian.takita@gmail.com>
2021-07-15 00:20:54 -0400

    ## 12.0.1
    ### Patch Changes
    
    - 2a6971f80: fix: "type": "module"
    - fix: "type": "module"
    - Updated dependencies [2a6971f80]
    - Updated dependencies [undefined]
    - @ctx-core/object@21.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # @ctx-core/uri
 
+## 12.0.1
+
+### Patch Changes
+
+- 2a6971f80: fix: "type": "module"
+- fix: "type": "module"
+- Updated dependencies [2a6971f80]
+- Updated dependencies [undefined]
+  - @ctx-core/object@21.0.1
+
 ## 12.0.0
 
 ### Major Changes
--- src/join_url.ts
+++ src/join_url.ts
@@ -1,4 +1,4 @@
-import type { url_segment_T } from './url_segment_T'
+import type { url_segment_T } from './url_segment_T.js'
 export function join_url(url_segment_a:url_segment_T[]):string {
 	return url_segment_a.reduce((url, url_segment)=>{
 		const url_segment_str = url_segment.toString()
